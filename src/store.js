2759ba769469ed6d5faeb235e47540ac42cc67ec
Siddharthan Dileep <siddharthandileep@gmail.com>
2022-12-10 08:45:53 +0530

    All functionalities complete

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit";
 import cartReducer from "./feature/cart-slice"
 import productsReducer from "./feature/products-slice"
+import checkoutReducer from "./feature/checkout-slice";
 import categoriesReducer from "./feature/categories-slice"
 
 export const store=configureStore({
     reducer:{
         cart:cartReducer,
         products:productsReducer,
-        categories:categoriesReducer
+        categories:categoriesReducer,
+        checkout: checkoutReducer,
     }
 })
